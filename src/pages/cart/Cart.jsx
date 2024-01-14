cbb9aba404910b8df0d512f65a3806943f23bad6
CodeBustler <codebustler360online@gmail.com>
2024-01-14 13:07:43 +0530

    Cart Page Complete

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,61 +1,71 @@
 import { useContext, useEffect, useState } from "react";
 import { MyContext } from "../../App";
-// REDUX
 import { useDispatch, useSelector } from "react-redux";
 import { deleteFromCart } from "../../redux/cartSlice";
-// ROUTER
 import { Link, useNavigate } from "react-router-dom";
-// ICONS
-import { RiDeleteBin6Fill } from "react-icons/ri";
-import { FaBagShopping } from "react-icons/fa6";
-import emptyCart from "../../assets/empty_cart.jpg";
 import { toast } from "react-toastify";
 import { MdDeleteForever } from "react-icons/md";
-import { FaMinus } from "react-icons/fa6";
-import { FaPlus } from "react-icons/fa";
-
-//-----------------------------------------------------------------------
+import { FaBagShopping, FaMinus, FaPlus } from "react-icons/fa6";
+import emptyCart from "../../assets/empty_cart.jpg";
 
 function Cart() {
 	const cartItems = useSelector((state) => state.cart);
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
-	const [productCount, setProductCount] = useState(1);
 	const { userName, admin, numberWithCommas, setCartAnimate, scrollToTop } =
 		useContext(MyContext);
 
 	useEffect(() => {
 		scrollToTop();
 	}, []);
-	//-----------------------------------------------------------------------
 
+	// Product counts state
+	const [productCounts, setProductCounts] = useState(
+		cartItems.map(() => 1), // Initialize counts for each item to 1
+	);
+
+	const totalItems = productCounts.reduce(
+		(total, element) => total + element,
+		0,
+	);
 	// SUBTOTAL PRICE
 	const cartProductsPrice = cartItems.reduce(
-		(total, item) => total + parseInt(item.price),
+		(total, item, index) =>
+			total + parseInt(item.price) * productCounts[index],
 		0,
 	);
 
 	// TOTAL PRICE
-	const totalAmout = cartProductsPrice + 20 + 100;
-	console.log(cartProductsPrice);
+	const totalAmount = cartProductsPrice + 20 + 100;
 
-	// DLETE ITEM FROM CART
-	const deleteItem = (item) => {
+	// DELETE ITEM FROM CART
+	const deleteItem = (item, index) => {
 		toastRed();
 		dispatch(deleteFromCart(item));
+		// Remove the count for the deleted item
+		setProductCounts((prevCounts) =>
+			prevCounts.filter((_, i) => i !== index),
+		);
 	};
+
 	function toastRed() {
 		toast.dismiss("Item Removed! ");
 	}
 
 	// HANDLE PRODUCT COUNTER
-	const handleProductCountMinus = () => {
-		setProductCount((prevCount) => prevCount - 1);
+	const handleProductCountMinus = (index) => {
+		setProductCounts((prevCounts) =>
+			prevCounts.map((count, i) =>
+				i === index ? Math.max(count - 1, 1) : count,
+			),
+		);
 	};
-	const handleProductCountPlus = () => {
-		setProductCount((prevCount) => prevCount + 1);
+
+	const handleProductCountPlus = (index) => {
+		setProductCounts((prevCounts) =>
+			prevCounts.map((count, i) => (i === index ? count + 1 : count)),
+		);
 	};
-	//-----------------------------------------------------------------------
 
 	return (
 		<>
@@ -79,96 +89,105 @@ function Cart() {
 					</Link>
 				</div>
 			) : (
-				// ----------------------------------------------------------
 				<>
-					{/*CART CONTET */}
 					<h1 className="text-2xl font-bold  text-center md:text-left mb-5 underline underline-offset-8">
 						Cart Items
 					</h1>
 					<section className="">
-						{/*CARTS CONTAINER*/}
 						<div className="flex flex-col-reverse  md:flex-row items-center md:items-start justify-center p-1 md:p-5 ">
-							{/* CART ITEMS CONTAINER */}
 							<div className="w-full md:w-2/3 ">
-								{cartItems.map((item) => {
-									return (
-										<div
-											key={item.id}
-											className="md:w-[90%] p-5 mb-7 rounded-lg flex flex-col md:flex-row gap-8 relative items-center transition border bg-gray-100 hover:shadow-xl hover:border min-h-[280px] "
-										>
-											<img
-												src={item.img1}
-												alt="product"
-												className="rounded-md cursor-pointer w-[60%] md:w-[150px] object-contain hover:scale-105 transition bg-white mt-5 md:mt-0"
-												title="Tap to know more..."
-												onClick={() => {
-													navigate(
-														`/ProductDetail/${item.id}`,
-													);
-													scrollToTop();
-												}}
-											/>
-											<div className="flex flex-col justify-between ">
-												<div className="">
-													<h3 className="font-semibold text-xl">
-														{item.title}
-													</h3>
-													<p className="mt-2 text-gray-500">
-														{item.description.slice(
-															0,
-															200,
-														)}
-														...
-													</p>
-													<h3 className="font-semibold text-xl mt-3">
+								{cartItems.map((item, index) => (
+									<div
+										key={item.id}
+										className="md:w-[90%] p-5 mb-7 rounded-lg flex flex-col md:flex-row gap-8 relative items-center transition border bg-gray-100 hover:shadow-xl hover:border min-h-[280px] "
+									>
+										<img
+											src={item.img1}
+											alt="product"
+											className="rounded-md cursor-pointer w-[60%] md:w-[150px] object-contain hover:scale-105 transition bg-white mt-5 md:mt-0"
+											title="Tap to know more..."
+											onClick={() => {
+												navigate(
+													`/ProductDetail/${item.id}`,
+												);
+												scrollToTop();
+											}}
+										/>
+										<div className="flex flex-col justify-between ">
+											<div className="">
+												<div className="font-semibold text-xl mb-1">
+													{item.title}
+												</div>
+												<small className="capitalize text-gray-400 px-3  pb-[3px] pt-[2px] bg-white rounded-xl ">
+													{item.subCategory.replace(
+														/_/g,
+														" ",
+													)}
+												</small>
+												<p className="mt-3 text-gray-500">
+													{item.description.slice(
+														0,
+														200,
+													)}
+													...
+												</p>
+												<div className="font-semibold text-xl mt-3">
+													₹{" "}
+													{item.price
+														? numberWithCommas(
+																item.price,
+														  )
+														: ""}{" "}
+													&nbsp;
+													<small className="line-through text-gray-400">
 														₹{" "}
-														{item.price
+														{item.actualPrice
 															? numberWithCommas(
-																	item.price,
+																	item.actualPrice,
 															  )
 															: ""}
-													</h3>
+													</small>
 												</div>
-												<div className="flex items-center justify-between mt-7">
-													<div className=" flex items-center justify-center bg-white  border shadow-xl rounded-lg bg-blue-400 text-lg  ">
-														<FaMinus
-															className=" cursor-pointer w-[35px] px-2"
-															onClick={() =>
-																handleProductCountMinus(
-																	item.price,
-																)
-															}
-														/>
-														<div className="px-5 bg-white                                    ">
-															{productCount}
-														</div>
-														<FaPlus
-															className=" cursor-pointer w-[35px] px-3"
-															onClick={() =>
-																handleProductCountPlus(
-																	item.price,
-																)
-															}
-														/>
-													</div>
-													<MdDeleteForever
-														className="text-4xl text-red-500 cursor-pointer hover:scale-150 transition"
+											</div>
+											<div className="flex items-center justify-between mt-7">
+												<div className=" flex items-center justify-center  shadow-xl rounded-lg text-lg bg-orange-400">
+													<FaMinus
+														className=" cursor-pointer w-[35px] px-2"
 														onClick={() =>
-															deleteItem(item)
+															handleProductCountMinus(
+																index,
+															)
+														}
+													/>
+													<div className="px-6 bg-white select-none">
+														{productCounts[index]}
+													</div>
+													<FaPlus
+														className=" cursor-pointer w-[35px] px-3"
+														onClick={() =>
+															handleProductCountPlus(
+																index,
+															)
 														}
 													/>
 												</div>
+												<MdDeleteForever
+													className="text-4xl text-red-500 cursor-pointer hover:scale-150 transition"
+													onClick={() =>
+														deleteItem(item, index)
+													}
+												/>
 											</div>
 										</div>
-									);
-								})}
+									</div>
+								))}
 							</div>
 
-							{/* BILLING */}
-							<div className="border w-full md:w-1/3 p-6 rounded-lg bg-gray-100 shadow-xl hover:shadow-2xl transition mb-12">
+							{/*BUY SECTION*/}
+							<div className="border w-full md:w-1/3 p-6 rounded-lg bg-gray-100 shadow-xl hover:shadow-2xl transition mb-12 md:sticky md:top-[120px] select-none">
 								<div className="flex justify-between text-lg font-semibold">
-									<div>
-										Subtotal ({cartItems.length} Items)
+									<div className="">
+										Subtotal ({totalItems} Items)
 									</div>
 									<div>
 										₹{" "}
@@ -192,8 +211,8 @@ function Cart() {
 									<div>
 										{" "}
 										₹{" "}
-										{totalAmout
-											? numberWithCommas(totalAmout)
+										{totalAmount
+											? numberWithCommas(totalAmount)
 											: ""}
 									</div>
 								</div>
@@ -205,7 +224,7 @@ function Cart() {
 									<span>Send as a gift. Custom message </span>
 								</div>
 								<button className="rounded-lg text-center w-full mt-7 bg-orange-400 py-2 font-semibold">
-									Proceed to Buy ({cartItems.length} Items)
+									Proceed to Buy ({totalItems} Items)
 								</button>
 							</div>
 						</div>
